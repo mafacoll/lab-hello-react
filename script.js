7c5595f210a9da154b2152c9d401f9ae9a91908d
Maria Farran Colell <mafacoll@hotmail.com>
2021-11-29 14:13:29 +0100

    lab

--- script.js
+++ script.js
@@ -17,36 +17,64 @@ function MenuImage(){
 
 }
 
-function Title(){
-    return <h1>Say hello to ReactJS</h1>
-
-}
 
 function Introduction(){
-    return <h2>You will learn how to use <br/>
+    return <div>
+    <h1>Say hello to ReactJS</h1>
+    <h2>You will learn how to use <br/>
         the most popular frontend library, <br/>
         and become a super Ninja developer. 
     </h2>
+    </div> 
 }
+
+   
 
 
 function DeclarativeImage(){
-    return <img src="icon1.png" alt="Ironhack Logo"/>
+    return <div>
+    <img src="icon1.png" alt="Declarative Image"/>
+    <h1> Declarative </h1>
+    <h2> React makes it <br/>
+    painless to create<br/>
+    interactive UIs. 
+    </h2>
+    </div>
 
 }
 
 function ComponentsImage(){
-    return <img src="icon2.png" alt="Ironhack Logo"/>
-
+    return<div> 
+    <img src="icon2.png" alt="Components Image"/>
+    <h1> Components </h1>
+    <h2> Build encapsulated <br/>
+    components that<br/>
+    manage their state. 
+    </h2>
+    </div>
 }
 
 function SingleImage(){
-    return <img src="icon3.png" alt="Ironhack Logo"/>
-
+    return <div>
+    <img src="icon3.png" alt="SingleImage"/>
+    <h1> Single-Way </h1>
+    <h2> A set of immutable <br/>
+    values are passed<br/>
+    to the compnent's.
+    </h2>
+    </div>
 }
 
+
 function JsxImage(){
-    return <img src="icon4.png" alt="Ironhack Logo"/>
+    return <div>
+    <img src="icon4.png" alt="JSX Logo"/>
+    <h1> JSX </h1>
+    <h2> Statically-typed <br/>
+    designed to run on<br/>
+    modern browsers.
+    </h2>
+    </div>
 
 }
 
@@ -55,7 +83,6 @@ function MyApp(){
         <div>
         <IronhackImage/>
         <MenuImage/>
-        <Title/>
         <Introduction/>
         <DeclarativeImage/>
         <ComponentsImage/>
